123fd9addc416fd3103e5deae3a49b59d6be4487
RylovNigami <mendezr1700@gmail.com>
2026-01-04 22:49:39 -0400

    update to master

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,15 +1,18 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export const config: TypeOrmModuleOptions = {
 
-    type:'postgres',
-    username:'postgres',
-    password:'1234',
-    port:5432,
-    host:'localhost',
-    database:'ong',
-    synchronize:true,
-    entities:['dist/**/*.entity{.ts,.js}'],
-
+    type: 'postgres',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_DATABASE,
+    synchronize: process.env.SYNCHRONIZE === 'true',
+    entities: ['dist/**/*.entity{.ts,.js}'],
 }
 
 
